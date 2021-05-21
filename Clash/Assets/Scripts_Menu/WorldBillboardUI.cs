beea1df7ff7988f6305e5f7470281fa0e21ddc1b
reborn1000 <wil20e10@gmail.com>
2021-05-20 23:15:09 -0300

    ++

---
+++ Clash/Assets/Scripts_Menu/WorldBillboardUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[ExecuteAlways]
+
+public class WorldBillboardUI : MonoBehaviour
+{
+
+    private void LateUpdate()
+    {
+
+        Vector3 lookAtTarget = transform.position + Camera.main.transform.forward;
+        transform.LookAt(lookAtTarget, Camera.main.transform.up); 
+    }
+
+
+}
