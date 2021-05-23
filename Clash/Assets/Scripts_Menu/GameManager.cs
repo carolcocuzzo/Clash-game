ee6d9cca63df1c3f0f0f88d34093d033e7830ad7
reborn1000 <wil20e10@gmail.com>
2021-05-23 18:59:49 -0300

    ++

---
+++ Clash/Assets/Scripts_Menu/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+
+ 
+{
+
+    public GameObject MenuPause;
+    public GameObject ButtonPause;
+    public bool isPaused = false;   
+
+
+    public void Pause()
+    {
+        
+
+
+        if (isPaused)
+        {
+            
+            MenuPause.SetActive(false);
+            isPaused = false;
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+            ButtonPause.SetActive(true);
+            
+        }
+        else
+        {
+            MenuPause.SetActive(true);
+            isPaused = true;
+            AudioListener.pause = true;
+            Time.timeScale = 0;
+            ButtonPause.SetActive(false);
+
+        }
+
+        
+
+    }
+
+    public void RestartScene()
+
+    {
+        MenuPause.SetActive(false);
+        isPaused = false;
+        //GameOverPainel.gameObject.SetActive(false);
+        //restartButton.gameObject.SetActive(false);
+        Time.timeScale = 1; // despausar jogo
+        AudioListener.pause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+
+
+    }
+
+    public void VoltaraoMenu()
+
+
+    {
+        AudioListener.pause = false;
+        isPaused = false;
+        SceneManager.LoadScene("Menu");
+
+    }
+
+
+}
